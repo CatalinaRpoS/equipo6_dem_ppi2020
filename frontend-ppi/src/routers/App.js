91c0de8c2be0554196292650d48f7da9b75ad399
Jacobo Garcés Oquendo <66318565+JacoboGarcesO@users.noreply.github.com>
2020-09-24 10:47:45 -0500

    Avance Front-End

--- frontend-ppi/src/routers/App.js
+++ frontend-ppi/src/routers/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Bienvenida from '../pages/Bienvenida';
+import Registrarse from '../pages/Registrarse';
+import IniciarSesion from '../pages/IniciarSesion';
+import OlvidasteContraseña from '../pages/OlvidasteContraseña';
+import ConfiguraContraseña from '../pages/ConfiguraCont';
+import ContinuarSinCuenta from '../pages/ContinuarSinCuenta';
 
 function App() {
   return (
     <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Bienvenida}/>
+      <Route exact path="/registro" component={Registrarse}/>
+      <Route exact path="/iniciar-sesion" component={IniciarSesion}/>
+      <Route exact path="/olvidaste-contraseña" component={OlvidasteContraseña}/>
+      <Route exact path="/configura-contraseña" component={ConfiguraContraseña}/>
+      <Route exact path="/continuar-sin-cuenta" component={ContinuarSinCuenta}/>
     </Switch>
     </BrowserRouter>
   );
