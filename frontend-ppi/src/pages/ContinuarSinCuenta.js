c42d477a0e2eb922ff52d75514c3f4516d0470e4
Jacobo Garcés Oquendo <66318565+JacoboGarcesO@users.noreply.github.com>
2020-09-24 10:32:55 -0500

    Avance Front-End

---
+++ frontend-ppi/src/pages/ContinuarSinCuenta.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavbarVacio from '../components/navbar_vacio';
+
+class ContinuarSinCuenta extends React.Component{
+  render(){
+    return(
+      <div>
+        <NavbarVacio/>
+        
+      </div>
+    );
+  }
+
+}
+
+export default ContinuarSinCuenta;
