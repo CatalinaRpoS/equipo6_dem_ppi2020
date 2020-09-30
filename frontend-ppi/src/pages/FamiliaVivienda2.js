5d37a4a1a23b38c77e10932ec9ee41bca5148a52
Jacobo Garcés Oquendo <66318565+JacoboGarcesO@users.noreply.github.com>
2020-09-30 18:01:01 -0500

    Avance Front-End

--- frontend-ppi/src/pages/FamiliaVivienda2.js
+++ frontend-ppi/src/pages/FamiliaVivienda2.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import NavbarLleno from '../components/navbar_lleno';
+import ImagenTitulo from '../components/imagen_titulo';
+import Casa from '../images/casa.jpg';
 
 class FamiliaVivienda2 extends React.Component{
   render(){
     return(
       <div>
-        <NavbarLleno/>  
+        <NavbarLleno/>
+        <ImagenTitulo
+          imagen={Casa}
+          texto="¿NO QUIERES CREAR UNA CUENTA?"
+          texto2="¡Entonces échale un vistazo a los siguientes tips de ahorro de energía!"
+        />  
       </div>
     );
   }
